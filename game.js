0c55824ba6385a3417ae229034d3c4671ff92afb
niharika <niharikajha1702@gmail.com>
2025-08-14 21:21:50 +0530

    modified

--- game.js
+++ game.js
@@ -1,17 +1,36 @@
-let gameseq=[];
-let userseq=[];
+let gameseq = [];
+let userseq = [];
 let started = false;
-let level =0;
+let level = 0;
+let h2 = document.querySelector("h2");
+let btns =["yellow", "red", "green", "pink"];
 
-document.addEventListener("keypress ", function(){
-    console.log("game started");
-// if (started=false){
-//     console.log("game started")
-// started=true;
-//     levelup();
-// }
- });
+document.addEventListener("keypress", function () {
 
-//  function levelup(){
-//     level ++;
-//  }
+    if (started == false) {
+        console.log("game started");
+        started = true;
+        levelup();
+    }
+});
+
+function levelup() {
+    level++;
+    h2.innerText = `level ${level}`
+let randIdx = Math.floor(Math.random()*3);
+let randcolor =btns[randIdx];
+let randbtn=document.querySelector(`.${randcolor}`);
+console.log(randbtn);
+console.log(randIdx);
+console.log(randcolor);
+     btnflash(randbtn);
+}
+ function btnflash(box){
+    box.classList.add("flash");
+  setTimeout(function(){
+box.classList.remove("flash");
+  },250
+
+);
+}
+ 
