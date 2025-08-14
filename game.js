b03a93dd4d5464358c5a49d18a4729cea854da98
niharika <niharikajha1702@gmail.com>
2025-08-14 20:55:22 +0530

    modified

---
+++ game.js
@@ -0,0 +1,17 @@
+let gameseq=[];
+let userseq=[];
+let started = false;
+let level =0;
+
+document.addEventListener("keypress ", function(){
+    console.log("game started");
+// if (started=false){
+//     console.log("game started")
+// started=true;
+//     levelup();
+// }
+ });
+
+//  function levelup(){
+//     level ++;
+//  }
